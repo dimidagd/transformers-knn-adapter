ba74abb131488a722ed94d1f536dea9f82f57b12
dimidagd <46669905+dimidagd@users.noreply.github.com>
2026-03-18 14:02:57 +0000

    feat(knn): add Trainer KNN callback

--- src/transformers_knn_adapter/__init__.py
+++ src/transformers_knn_adapter/__init__.py
@@ -2,11 +2,13 @@
 
 from .dinov2_arcface import Dinov2ForImageClassificationWithArcFaceLoss
 from .freeze_schedule_callback import FreezeScheduleCallback
+from .knn_callback import KNNCallback
 from .knn_image_pipeline import KNNImageClassificationPipeline, pipeline
 
 __all__ = [
     "Dinov2ForImageClassificationWithArcFaceLoss",
     "FreezeScheduleCallback",
+    "KNNCallback",
     "KNNImageClassificationPipeline",
     "pipeline",
 ]
